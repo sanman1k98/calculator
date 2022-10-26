0971e3639b35c0e99ef9c553c05907b0d8088cfc
Sandeep Dilip <34922961+sanman1k98@users.noreply.github.com>
2022-10-26 16:30:52 -0400

    Respond with status code 500 when dividing by 0
    Use our "Input" type when destructuring the NextApiRequest.

--- next/src/pages/api/div.ts
+++ next/src/pages/api/div.ts
@@ -1,15 +1,17 @@
 import type { NextApiRequest, NextApiResponse } from "next";
+import type { Input } from "../../interfaces/Input"
 
 export default (req: NextApiRequest, res: NextApiResponse) => {
 
-  // TODO: create a type for the body
-  const {
-    body: { left, right },
-    method,
-  } = req
+  const { body, method }: { body: Input, method?: string | undefined } = req
 
   if (method === 'POST') {
-    res.status(200).json({ result: left / right })
+    const result = body.x / body.y
+    if ( typeof result !== "number" ) {
+      res.status(500).end({ error: "Not a Number" })
+    }
+    // Infinity, NaN, and null are all considered null when using JSON.stringify()
+    res.status(200).send({ result: result })
   } else {
     res.setHeader('Allow', ['POST'])
     res.status(405).end(`Method ${method} Not Allowed`)
