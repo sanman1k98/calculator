5f18faea22168bbed825b6cc2f9f074da3cfb995
Sandeep Dilip <34922961+sanman1k98@users.noreply.github.com>
2022-10-26 17:44:31 -0400

    Add another test for float division
    I forgot that JavaScript does not do floor division by default like
    Java. Happy to see it passed without me changing the implementation.

--- next/tests/api/div.spec.ts
+++ next/tests/api/div.spec.ts
@@ -17,3 +17,13 @@ test('should not result in a number when dividing by zero', async ({ request }) 
   expect(!result.ok())
   expect(result.status() === 500)
 })
+
+test('should return 1.5 when dividing 3 with 2', async ({ request }) => {
+  const result = await request.post('/api/div', {
+    data: { x: 3, y: 2 }
+  })
+  expect(result.ok())
+  expect(await result.json()).toEqual({
+    result: 1.5
+  })
+})
