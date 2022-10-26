1214fa9b30bf619b4e8160e3939796b5bfcd367f
Sandeep Dilip <34922961+sanman1k98@users.noreply.github.com>
2022-10-26 00:20:50 -0400

    Add API route `/api/div`
    Make the test pass.
    
    TODO: refine the assertions in the second test so we're sure what kind
    of data is being returned when we divide by zero.

---
+++ next/src/pages/api/div.ts
@@ -0,0 +1,17 @@
+import type { NextApiRequest, NextApiResponse } from "next";
+
+export default (req: NextApiRequest, res: NextApiResponse) => {
+
+  // TODO: create a type for the body
+  const {
+    body: { left, right },
+    method,
+  } = req
+
+  if (method === 'POST') {
+    res.status(200).json({ result: left / right })
+  } else {
+    res.setHeader('Allow', ['POST'])
+    res.status(405).end(`Method ${method} Not Allowed`)
+  }
+}
