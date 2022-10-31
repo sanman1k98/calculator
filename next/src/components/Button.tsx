d2593e00eeb9e4c33fb92166acc1db16075176fb
Likith Giddi <likith.giddi@gmail.com>
2022-10-31 15:17:36 -0400

    commit created button component, has css styles

--- next/src/components/Button.tsx
+++ next/src/components/Button.tsx
@@ -1,13 +1,14 @@
 type ButtonProps = {
-    text: string,
-    onClick: () => void,
+    onClick?: () => void,
     className?: string,
+    children?: any,
 }
+
 
 export default function Button(props: ButtonProps) {
     return (
-        <button onClick={props.onClick} className={props.className}>
-            {props.text}
+        <button onClick={props.onClick} className={`hover:bg-calcOrange-200 text-white py-2 ${props.className || ""}`}>
+            {props.children}
         </button>
     )
 }
